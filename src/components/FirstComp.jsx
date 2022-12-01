cbcf27968d6b1b581850e85ea152ce4b15641375
nati070 <nnati070@gmail.com>
2022-12-01 17:51:12 +0200

    final

--- src/components/FirstComp.jsx
+++ src/components/FirstComp.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import "./FirstComp.css"
-
-
-
+import "./DesignComp.css"
 
 const FirstComp = () => {
     return (
         <div className='main-title'>
-             <span class="title-word title-word-1">My</span>
-             <span class="title-word title-word-2">First</span>
-             <span class="title-word title-word-3">Component</span>
+             <span className="title-word title-word-1">My</span>
+             <span className="title-word title-word-2">First</span>
+             <span className="title-word title-word-3">Component</span>
         </div>
     );
 };
--- src/components/SecondComp.jsx
+++ src/components/SecondComp.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import "./FirstComp.css"
-
-
-
+import "./DesignComp.css"
 
 const SecondComp = () => {
     return (
         <div className='main-title'>
-             <span class="title-word title-word-1">My</span>
-             <span class="title-word title-word-2">Second</span>
-             <span class="title-word title-word-3">Component</span>
+             <span className="title-word title-word-1">My</span>
+             <span className="title-word title-word-2">Second</span>
+             <span className="title-word title-word-3">Component</span>
         </div>
     );
 };
